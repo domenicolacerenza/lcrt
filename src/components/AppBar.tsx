600ab5bae1703e1ab8024df33750d7088fba63dc
Domenico Lacerenza <domenicolacerenza@LCRT-MacBook.local>
2023-08-21 12:49:41 +0200

    ready for publication

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -18,7 +18,7 @@ export const AppBar: React.FC = () => {
     <div>
       {/* NavBar / Header */}
       <div className="navbar w-full">
-      <img className='w-40 mx-auto' src="/img/logo.png" /> 
+      <img className='w-40 mx-auto' src="/img/LOGO.png" /> 
         <div className="hidden md:inline-flex justify-items gap-6">
             <ul className="absolute top-4 right-4 menu menu-horizontal text-2xl rounded-box bg-base-100 ">
               <li>
