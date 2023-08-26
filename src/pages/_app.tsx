4bb7e13eaad418bc15dc55be9dc2e39bedf11e6f
Domenico Lacerenza <domenicolacerenza@LCRT-MacBook.local>
2023-08-26 17:45:08 +0200

    pwa fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,24 +23,16 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-      <meta name="msapplication-TileColor" content="#069072" />
+      <meta name="msapplication-TileColor" content="#069072 " />
       <meta name="msapplication-tap-highlight" content="no" />
       <meta name="theme-color" content="#000000" />
 
-      <link rel="apple-touch-icon" href="/public/favicon.ico" />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
       
-      <link rel="icon" type="image/png" sizes="any" href="/public/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="any" href="/favicon.ico" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
-      <meta name="twitter:card" content="LCRT XYZ" />
-      <meta name="twitter:url" content="https://www.lcrt.xyz" />
-      <meta name="twitter:title" content="LCRT XYZ" />
-      <meta name="twitter:description" content="Nico Lacerenza - Visual Sound Artist" />
-      <meta name="twitter:image" content="https://www.lcrt.xyz/img/ab6761670000ecd44113acecee3e7fbae911e540.jpeg" />
-      <meta name="twitter:creator" content="@lcrtbeats" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="LCRT XYZ" />
       <meta property="og:description" content="Nico Lacerenza - Visual Sound Artist" />
